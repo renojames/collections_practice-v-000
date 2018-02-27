8e166e3c8fb1979dcdf3db61ab2a025b61e46f20
Reno Sorensen <renojames19@gmail.com>
2018-02-27 04:54:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
 
+def sort_array_asc(array)
+  array = array.sort
+end
+
+def sort_array_desc(array)
+  array = array.sort
+  array = array.reverse
+end
+
+def sort_array_char_count(array)
+  array = array.sort do | a, b |
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  x = array[2]
+  array[2] = array[1]
+  array[1] = x
+  array
+end
+
+def reverse_array(array)
+  array = array.reverse
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |i|
+    i.slice!(2)
+    i.insert(2, '$')
+    kesha_array << i
+  end
+  kesha_array
+end
